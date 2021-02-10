ea5c13cc2776d48fa56b8c4429e3403436cd20af
Greg Doud <gpdoud@gmail.com>
2021-02-09 19:32:19 -0500

    Add project files.

---
+++ bcms-cli/Args.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doudsystems.Bcms.Cli {
+    
+    internal class Args {
+
+        static Dictionary<string, string> parms = new Dictionary<string, string>();
+
+        internal static IDictionary<string, string> Parse(string[] args) {
+
+            string key = null;
+            var firstTime = true;
+
+            foreach(var arg in args) {
+                if(firstTime) {
+                    AddParm("cmd", arg);
+                    firstTime = !firstTime;
+                    continue;
+                }
+                if(arg.StartsWith("--")) {
+                    if(key != null) { // still have a key with no data
+                        AddParm(key, null);
+                        key = null;
+                    }
+                    key = arg;
+                } else { // no dashes, must be data; write key value
+                    AddParm(key, arg);
+                    key = null;
+                }
+            }
+            if(key != null) {
+                AddParm(key, null);
+            }
+            return parms;
+        }
+        private static void AddParm(string key, string val) {
+            parms.Add(key, val);
+        }
+    }
+}
---
+++ bcms-cli/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Doudsystems.Bcms.Cli {
+
+    class Program {
+    
+        static void Main(string[] args) {
+            var parms = Args.Parse(args);
+            foreach(var parm in parms) {
+                Console.WriteLine($"{parm.Key} [{(parm.Value == null ? "null" : parm.Value)}]");
+            }
+        }
+    }
+}
